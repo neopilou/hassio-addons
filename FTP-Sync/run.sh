495b167accd5b2882c72e8d8a659d56415e046a4
neopilou <neonicus@free.fr>
2020-05-01 11:05:03 +0200

    Create FTP-Sync

---
+++ FTP-Sync/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+echo "[Info] Starting FTP Backup docker!"
+
+CONFIG_PATH=/data/options.json
+ftpprotocol=$(jq --raw-output ".ftpprotocol" $CONFIG_PATH)
+ftpserver=$(jq --raw-output ".ftpserver" $CONFIG_PATH)
+ftpport=$(jq --raw-output ".ftpport" $CONFIG_PATH)
+ftpbackupfolder=$(jq --raw-output ".ftpbackupfolder" $CONFIG_PATH)
+ftpusername=$(jq --raw-output ".ftpusername" $CONFIG_PATH)
+ftppassword=$(jq --raw-output ".ftppassword" $CONFIG_PATH)
+addftpflags=$(jq --raw-output ".addftpflags" $CONFIG_PATH)
+KEEP_LAST=$(jq --raw-output ".keep_last // empty" $CONFIG_PATH)
+
+ftpurl="$ftpprotocol://$ftpserver:$ftpport/$ftpbackupfolder/"
+credentials=""
+if [ "${#ftppassword}" -gt "0" ]; then
+	credentials="-u $ftpusername:$ftppassword"
+fi
+	
+hassconfig="/config"
+hassbackup="/backup"
+tarpath="$hassbackup/*.tar"
+
+echo "[Info] trying to upload $tarpath to $ftpurl"
+curl $addftpflags $credentials -T $tarpath $ftpurl
+echo "[Info] Finished ftp backup"
+
+if [[ "$KEEP_LAST" ]]; then
+    echo "[Info] keep_last option is set, cleaning up files..."
+    python3 /keep_last.py "$KEEP_LAST"
+fi
