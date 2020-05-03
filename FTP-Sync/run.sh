484a51e3e6f81c758fc46191548f8279f18eb903
Neonicus <45233613+neopilou@users.noreply.github.com>
2020-05-03 17:31:44 +0200

    fix

--- FTP-Sync/run.sh
+++ FTP-Sync/run.sh
@@ -36,12 +36,12 @@ while read -r msg; do
                 cd /backup
 		for f in *.tar; do
 			ftpfile="$protocol://$server:$port/$path/$f"
-			if ( curl $credentials -o/dev/null -sfI "$ftpfile" == 0 ); then
-				echo "[Info] File $f already exist on $ftpurl and was not uploaded"
-			else
+			#if ( curl $credentials -o/dev/null -sfI "$ftpfile" == 0 ); then
+				#echo "[Info] File $f already exist on $ftpurl and was not uploaded"
+			#else
 				echo "[Info] trying to upload $f to $ftpurl"
-				curl $credentials -T $f $ftpurl
-			fi
+				curl $credentials -T -z $f $ftpfile
+			#fi
 		done
 		echo "[Info] Finished ftp backup"
 	fi
