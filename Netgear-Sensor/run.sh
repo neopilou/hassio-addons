0525930979a311e8872e7d51b5b6486cad07d65d
Neonicus <45233613+neopilou@users.noreply.github.com>
2020-11-04 16:27:08 +0100

    Update run.sh

--- Netgear-Sensor/run.sh
+++ Netgear-Sensor/run.sh
@@ -3,19 +3,3 @@
 echo "[Info] Starting Netgear Sensors docker!"
 
 echo "[Info] Starting uploading txbs and rxbs every $refresh_interval seconds"
-
-txbs1=""
-rxbs1=""
-
-txbs2=""
-rxbs2=""
-
-txbs=""
-rxbs=""
-
-while true; do
-	
-	txbs= txbs2 - txbs1
-	rxbs= rxbs2 - rxbs1	
-	
-done
