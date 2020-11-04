edc4e8b69d93da6eb6d8c2dc535a24621fabdb29
Neonicus <45233613+neopilou@users.noreply.github.com>
2020-11-04 15:32:54 +0100

    Add files via upload

---
+++ Netgear-Sensor/README.md
@@ -0,0 +1,37 @@
+# Hass.io Add-on: Netgear Sensors
+Publish txbs and rxbs in Mb/s of Netgear router on MQTT
+
+### About
+This add-on allows you to publish txbs and rxbs traffic of you netgear router on MQTT
+
+### Installation
+1. Add the add-ons repository to your Hass.io instance: `https://github.com/neopilou/hassio-addons`
+2. Install the FTP Sync add-on
+3. Configure the add-on 
+
+### Configuration
+
+|Parameter|Required|Description|
+|---------|--------|-----------|
+|`mqtt_server`|Yes|URL of your MQTT Server.|
+|`mqtt_port`|Yes|Port of your MQTT Server|
+|`mqtt_username`|Yes|Your MQTT Username|
+|`mqtt_password`|Yes|Your MQTT Password|
+|`topic`|Yes|Topic on your MQTT server where txbs and rxbs will be published|
+|`netgear_url`|Yes|Your Netgear Router URL|
+|`netgear_username`|Yes|Your Netgear Router Username|
+|`netgear_password`|Yes|Your Netgear Router Password|
+
+Example Configuration:
+```json
+{
+  "mqtt_server": "localhost",
+  "port": "'1883'",
+  "mqtt_username": "username",
+  "mqtt_password": "password",
+  "topic": "sensors/netgear",
+  "netgear_url": "192.168.0.1",
+  "netgear_username": "username",
+  "netgear_password": "password"
+}
+```
---
+++ Netgear-Sensor/run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "[Info] Starting Netgear Sensors docker!"
+
+CONFIG_PATH=/data/options.json
+
+mqtt_server=$(jq --raw-output ".mqtt_server" $CONFIG_PATH)
+mqtt_port=$(jq --raw-output ".mqtt_port" $CONFIG_PATH)
+topic=$(jq --raw-output ".topic" $CONFIG_PATH)
+mqtt_username=$(jq --raw-output ".mqtt_username" $CONFIG_PATH)
+mqtt_password=$(jq --raw-output ".mqtt_password" $CONFIG_PATH)
+netgear_url=$(jq --raw-output ".netgear_url" $CONFIG_PATH)
+netgear_username=$(jq --raw-output ".netgear_username" $CONFIG_PATH)
+netgear_password=$(jq --raw-output ".netgear_password" $CONFIG_PATH)
+
+ng_credentials="--user $netgear_username:$netgear_password"
+
+echo "[Info] Starting uploading txbs and rxbs every $refresh_interval seconds"
+
+txbsA=""
+rxbsA=""
+
+txbsB=""
+rxbsB=""
+
+txbs=""
+rxbs=""
+
+topic_txbs="$topic/txbs"
+topic_rxbs="$topic/rxbs"
+
+while true; do
+	
+	txbsA= $(curl $ng_credentials -s 'http://$netgear_url/RST_statistic.htm' | /bin/sed -n 's/var wan_txbs="\(.*\)";/\1/p') * 8 / 1000
+	rxbsA= $(curl $ng_credentials -s 'http://$netgear_url/RST_statistic.htm' | /bin/sed -n 's/var wan_txbs="\(.*\)";/\1/p') * 8 / 1000
+	
+	sleep 1
+	
+	txbsB= $(curl $ng_credentials -s 'http://$netgear_url/RST_statistic.htm' | /bin/sed -n 's/var wan_txbs="\(.*\)";/\1/p') * 8 / 1000
+	rxbsB= $(curl $ng_credentials -s 'http://$netgear_url/RST_statistic.htm' | /bin/sed -n 's/var wan_txbs="\(.*\)";/\1/p') * 8 / 1000
+	
+	txbs= txbsB - txbsA
+	rxbs= rxbsA - rxbsB
+	
+	/usr/bin/mosquitto_pub -h $mqtt_server -p $mqtt_port -u $mqtt_username -p $mqtt_password -t $topic_txbs -m $txbs
+	/usr/bin/mosquitto_pub -h $mqtt_server -p $mqtt_port -u $mqtt_username -p $mqtt_password -t $topic_rxbs -m $rxbs
+		
+	sleep 1
+	
+done
