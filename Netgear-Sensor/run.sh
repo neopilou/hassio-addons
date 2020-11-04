c332c338664368fef749092ab43816119b9c3538
Neonicus <45233613+neopilou@users.noreply.github.com>
2020-11-04 16:24:54 +0100

    Update run.sh

--- Netgear-Sensor/run.sh
+++ Netgear-Sensor/run.sh
@@ -2,19 +2,6 @@
 
 echo "[Info] Starting Netgear Sensors docker!"
 
-CONFIG_PATH=/data/options.json
-
-mqtt_server=$(jq --raw-output ".mqtt_server" $CONFIG_PATH)
-mqtt_port=$(jq --raw-output ".mqtt_port" $CONFIG_PATH)
-topic=$(jq --raw-output ".topic" $CONFIG_PATH)
-mqtt_username=$(jq --raw-output ".mqtt_username" $CONFIG_PATH)
-mqtt_password=$(jq --raw-output ".mqtt_password" $CONFIG_PATH)
-netgear_url=$(jq --raw-output ".netgear_url" $CONFIG_PATH)
-netgear_username=$(jq --raw-output ".netgear_username" $CONFIG_PATH)
-netgear_password=$(jq --raw-output ".netgear_password" $CONFIG_PATH)
-
-ng_credentials="--user $netgear_username:$netgear_password"
-
 echo "[Info] Starting uploading txbs and rxbs every $refresh_interval seconds"
 
 txbs1=""
@@ -25,9 +12,6 @@ rxbs2=""
 
 txbs=""
 rxbs=""
-
-topic_txbs="$topic/txbs"
-topic_rxbs="$topic/rxbs"
 
 while true; do
 	
