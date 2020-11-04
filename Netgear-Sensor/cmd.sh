6c6dc9afd83c72a4afd04c403a4bcf66db3a9994
Neonicus <45233613+neopilou@users.noreply.github.com>
2020-11-04 17:11:33 +0100

    Update cmd.sh

--- Netgear-Sensor/cmd.sh
+++ Netgear-Sensor/cmd.sh
@@ -36,13 +36,13 @@ while true; do
 	
 	sleep 1
 	
-	txbsB= $(curl $ng_credentials -s 'http://$netgear_url/RST_statistic.htm' | sed -n 's/var wan_txbs="\(.*\)";/\1/p') * 8 / 1000
-	rxbsB= $(curl $ng_credentials -s 'http://$netgear_url/RST_statistic.htm' | sed -n 's/var wan_txbs="\(.*\)";/\1/p') * 8 / 1000
+	txbsB= $(curl $ng_credentials -s 'http://$netgear_url/RST_statistic.htm' | /bin/sed -n 's/var wan_txbs="\(.*\)";/\1/p') * 8 / 1000
+	rxbsB= $(curl $ng_credentials -s 'http://$netgear_url/RST_statistic.htm' | /usr/bin/sed -n 's/var wan_txbs="\(.*\)";/\1/p') * 8 / 1000
 	
 	txbs= txbsB - txbsA
 	rxbs= rxbsB - rxbsA
 	
-	/usr/bin/mosquitto_pub -h $mqtt_server -p $mqtt_port -u $mqtt_username -p $mqtt_password -t $topic_txbs -m "150"
+	/usr/bin/mosquitto_pub -h $mqtt_server -p 1883 -u $mqtt_username -p $mqtt_password -t $topic_txbs -m "150"
 	/usr/bin/mosquitto_pub -h $mqtt_server -p $mqtt_port -u $mqtt_username -p $mqtt_password -t $topic_rxbs -m $rxbs
 		
 	sleep 1
