1dc23c2a3426da17dd45933948457e94eecbbebb
Neonicus <neonicus@free.fr>
2021-11-24 16:06:48 +0100

    Modified Samba for HA with WLAN0

---
+++ samba/rootfs/etc/cont-init.d/samba.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Prepare the Samba service for running
+# ==============================================================================
+declare password
+declare username
+declare interface
+export HOSTNAME
+
+# Check Login data
+if ! bashio::config.has_value 'username' || ! bashio::config.has_value 'password'; then
+    bashio::exit.nok "Setting a username and password is required!"
+fi
+
+# Read hostname from API or setting default "hassio"
+HOSTNAME=$(bashio::info.hostname)
+if bashio::var.is_empty "${HOSTNAME}"; then
+    bashio::log.warning "Can't read hostname, using default."
+    HOSTNAME="hassio"
+fi
+
+# Get default interface
+interface=$(bashio::network.name)
+
+bashio::log.info "Using hostname=${HOSTNAME} interface=${interface}"
+bashio::log.info "Using hostname=${HOSTNAME} interface=wlan0}"
+
+# Generate Samba configuration.
+jq ".interface = \"${interface}\" wlan0" /data/options.json \
+    | tempio \
+      -template /usr/share/tempio/smb.gtpl \
+      -out /etc/samba/smb.conf
+
+# Init user
+username=$(bashio::config 'username')
+password=$(bashio::config 'password')
+addgroup "${username}"
+adduser -D -H -G "${username}" -s /bin/false "${username}"
+# shellcheck disable=SC1117
+echo -e "${password}\n${password}" \
+    | smbpasswd -a -s -c "/etc/samba/smb.conf" "${username}"
