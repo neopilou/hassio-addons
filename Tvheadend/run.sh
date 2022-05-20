9f7048aebdd375c9c8d967eba07f0b28e80c5100
Neonicus <45233613+neopilou@users.noreply.github.com>
2022-05-20 11:27:20 +0200

    Update run.sh

--- Tvheadend/run.sh
+++ Tvheadend/run.sh
@@ -4,10 +4,6 @@ echo "[Info] Starting Tvheadend docker!"
 
 find / -name tvheadend
 
-/usr/bin/tvheadend -C -d -s
-
-while true
-  do echo "[Info] Executing"
-done
+exec /usr/bin/tvheadend -C -d -s
 
 echo "[Info] End !"
