bef0ebcb28ebb4afa56f22966123728ef6e7934a
Neonicus <neonicus@free.fr>
2022-05-25 11:31:09 +0200

    fix

--- Tvheadend/run.sh
+++ Tvheadend/run.sh
@@ -8,8 +8,6 @@ fi
 
 echo "[Info] Starting Tvheadend docker!"
 
-ffmpeg -loglevel fatal -i "rtsp://mafreebox.freebox.fr/fbxtv_pub/stream?namespace=1&service=294&flavour=hd" -vcodec copy -acodec copy -f mpegts -tune zerolatency pipe:1
-
 tvheadend -C -c /data
 
 echo "[Info] End !"
